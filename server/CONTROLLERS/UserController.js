dc1bfe6c9e1f56ed56da07abbb82f5567104a276
Gustavo <gustavo.biscaro@gmail.com>
2025-03-02 21:04:43 -0300

    image upload on database backend

--- server/CONTROLLERS/UserController.js
+++ server/CONTROLLERS/UserController.js
@@ -169,7 +169,9 @@ module.exports = class UserController {
 
         const { name, email, phone, password, confirmpassword } = req.body
 
-        let image = ''
+        if (req.file) {
+            user.image = req.file.filename
+        }
 
         // validando
         if (!name) {
@@ -198,14 +200,37 @@ module.exports = class UserController {
             res.status(422).json({ message: 'O telefone é obrigatório' })
             return
         }
-        if (!password) {
-            res.status(422).json({ message: 'A senha é obrigatória' })
+
+        user.phone = phone;
+
+        if (password != confirmpassword) {
+            res.status(422).json({ message: 'As senhas não conferem!' })
+            return
+        } else if (password === confirmpassword && password != null) {
+
+            const salt = await bcrypt.genSalt(12)
+            const passwordHash = await bcrypt.hash(password, salt)
+
+            user.password = passwordHash
+        }
+
+        try {
+            // dados atualizados
+            await User.findByIdAndUpdate(
+                { _id: user._id },
+                { $set: user },
+                { new: true },
+            )
+
+            res.status(200).json({
+                message: "Usuário atualizado com sucesso!",
+            })
+        } catch (err) {
+            res.status(500).json({ message: err })
             return
         }
-        if (!confirmpassword) {
-            res.status(422).json({ message: 'A confirmação de senha é obrigatória' })
-            return
-        }
+
+
 
     }
 }
--- server/ROUTES/UserRoutes.js
+++ server/ROUTES/UserRoutes.js
@@ -3,11 +3,12 @@ const router = require('express').Router()
 const UserController = require('../CONTROLLERS/UserController');
 
 const verifyToken = require('../HELPERS/verify-token')
+const { imageUpload } = require("../HELPERS/image-upload")
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
-router.patch('/edit/:id', verifyToken, UserController.editUser)
+router.patch('/edit/:id', verifyToken, imageUpload.single("image"), UserController.editUser)
 
 module.exports = router
